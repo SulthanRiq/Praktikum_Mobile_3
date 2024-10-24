a90d4211d400c9e0b0bcf2c12babf2f03a836fd8
SulthanRiq <sultanariq.yahoo.co.id@gmail.com>
2024-10-24 21:28:16 +0700

    Commit Pertama

---
+++ tugas/android/app/src/main/kotlin/sulthanariq17webmail/umm/ac/id/tugas/MainActivity.kt
@@ -0,0 +1,5 @@
+package sulthanariq17webmail.umm.ac.id.tugas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tugas/lib/app/modules/home/controllers/register_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  String _statusMessage = '';
+
+  Future<void> _registerUser() async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      setState(() {
+        _statusMessage = 'Registration successful: ${userCredential.user!.email}';
+      });
+    } catch (e) {
+      setState(() {
+        _statusMessage = 'Registration failed: $e';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _registerUser,
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 20),
+            Text(_statusMessage),
+          ],
+        ),
+      ),
+    );
+  }
+}
